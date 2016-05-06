07c2b60baed3911368f8807c8dc1efc07cab06cc
David Meza <davidmeza@outlook.com>
2016-05-06 13:16:00 -0400

    update puma file

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :deploy_to, '/home/deploy/strategicplanning'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :keep_releases, 5
+set :keep_releases, 3
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.0'
 
